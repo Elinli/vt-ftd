3eea1c106315753e8d4963e46c1150a355468260
shixx <shixiaoxiao@cechealth.cn>
2021-12-24 11:21:41 +0800

    fix: bugs

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,37 +4,37 @@ module.exports = {
   // ↓按照传统消息格式来验证
   extends: ['@commitlint/config-conventional'],
   // 自定义解析器
-  // parserPreset: {
-  //   // 解析器配置
-  //   parserOpts: {
-  //     // commit 提交头的规则限制
-  //     headerPattern: /^(\w*|[\u4e00-\u9fa5]*)(?:[\(\（](.*)[\)\）])?[\:\：] (.*)/,
-  //     // 匹配分组
-  //     headerCorrespondence: ['type', 'scope', 'subject'],
-  //     // 引用
-  //     referenceActions: [
-  //       'close',
-  //       'closes',
-  //       'closed',
-  //       'fix',
-  //       'fixes',
-  //       'fixed',
-  //       'resolve',
-  //       'resolves',
-  //       'resolved',
-  //     ],
-  //     // 对应issue要携带#符号
-  //     issuePrefixes: ['#'],
-  //     // 不兼容变更
-  //     noteKeywords: ['BREAKING CHANGE'],
-  //     fieldPattern: /^-(.*?)-$/,
-  //     revertPattern: /^Revert\s"([\s\S]*)"\s*This reverts commit (\w*)\./,
-  //     revertCorrespondence: ['header', 'hash'],
-  //     warn() {},
-  //     mergePattern: null,
-  //     mergeCorrespondence: null,
-  //   },
-  // },
+  parserPreset: {
+    // 解析器配置
+    parserOpts: {
+      // commit 提交头的规则限制
+      headerPattern: /^(\w*|[\u4e00-\u9fa5]*)(?:[\(\（](.*)[\)\）])?[\:\：] (.*)/,
+      // 匹配分组
+      headerCorrespondence: ['type', 'scope', 'subject'],
+      // 引用
+      referenceActions: [
+        'close',
+        'closes',
+        'closed',
+        'fix',
+        'fixes',
+        'fixed',
+        'resolve',
+        'resolves',
+        'resolved',
+      ],
+      // 对应issue要携带#符号
+      issuePrefixes: ['#'],
+      // 不兼容变更
+      noteKeywords: ['BREAKING CHANGE'],
+      fieldPattern: /^-(.*?)-$/,
+      revertPattern: /^Revert\s"([\s\S]*)"\s*This reverts commit (\w*)\./,
+      revertCorrespondence: ['header', 'hash'],
+      warn() {},
+      mergePattern: null,
+      mergeCorrespondence: null,
+    },
+  },
   // ↓自定义提交消息规则
   rules: {
     // ↓body以空白行开头
