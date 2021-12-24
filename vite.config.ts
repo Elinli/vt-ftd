7454d5577dff9df1cc421c16a36c2139272febff
shixx <shixiaoxiao@cechealth.cn>
2021-12-24 15:25:08 +0800

    build: add plugins and extends

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import { resolve } from 'path';
-
-function pathResolve(dir:string) {
-  return resolve(process.cwd(), '.', dir);
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    ElementPlus(),
+  ],
   resolve: {
     alias: [
       {
@@ -26,4 +39,4 @@ export default defineConfig({
   //     jsxFragment: 'Fragment',
   //     jsxInject: 'import { h } from "vue";',
   //   },
-});
+})
