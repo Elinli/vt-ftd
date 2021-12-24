f9dbc1216c16c33b682245b1ae93cb0aac7c8de6
shixx <shixiaoxiao@cechealth.cn>
2021-12-24 14:45:24 +0800

    feat: test ts

---
+++ src/enums/userEnum.ts
@@ -0,0 +1,14 @@
+export enum SexEnum {
+  MAN = 1,
+  WOMAN = 0,
+}
+
+export enum PositionEnum {
+  LEADER = 'leader',
+  STAFF = 'staff',
+}
+
+export enum DepartmentEnum {
+  'DEVP' = 'devp',
+  'TEST' = 'test',
+}
